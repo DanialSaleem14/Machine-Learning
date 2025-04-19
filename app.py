9607967b154ac7e2cabfa0e6a0c9ec530751b175
Danial Saleem <danialsaleemdgk1@gmail.com>
2025-04-20 02:41:23 +0500

    Added full project files

--- app.py
+++ app.py
@@ -4,3 +4,23 @@ import numpy as np
 
 
 st.title("My first app")
+
+st.write("This is a simple app")
+
+##create a simple dataframe
+
+df = pd.DataFrame({
+    'a': [1, 2, 3, 4],
+    'b': [5, 6, 7, 8],
+    'c': [9, 10, 11, 12]
+})
+
+st.write("This is a simple dataframe")
+st.write(df)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+st.line_chart(chart_data)
+
